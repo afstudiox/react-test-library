dea014b441e8a58282acefaf47d88d42cab04761
Andre Felipe <afelipes@gmail.com>
2022-03-22 23:21:08 -0300

    Requisito 4 - Teste o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+  renderWithRouter(<NotFound />);
+  const titleElement = screen.getByRole('heading', {
+    name: /page requested not found/i, level: 2 });
+  expect(titleElement).toBeDefined();
+});
+
+test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  renderWithRouter(<NotFound />);
+  const imgPoke = screen.getAllByRole('img');
+  expect(imgPoke[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
